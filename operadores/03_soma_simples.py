e6f12fe93b3d0acbb00f5c4ad3c8a1a8c7727a75
jose-programador <superacao.8379@gmail.com>
2026-04-19 17:55:02 -0300

    Criar 03_soma_simples.py
    Signed-off-by: jose-programador <superacao.8379@gmail.com>

---
+++ operadores/03_soma_simples.py
@@ -0,0 +1,10 @@
+A=int(input("Digite um número:"))
+B=int(input("Digite outro número:"))
+
+print("Resultado da soma é",A + B)
+
+print("Resultado da subtração é",A - B)
+
+print("Resultado da multiplicação é",A * B)
+
+print("Resultado do resto da divisão é", A % B )
