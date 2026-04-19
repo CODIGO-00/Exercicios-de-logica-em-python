9845f67cf046822eb8d22d8dff27b1ef97cee1eb
jose-programador <superacao.8379@gmail.com>
2026-04-19 17:39:36 -0300

    Delete basico/03_Calculo_da_media.py
    Signed-off-by: jose-programador <superacao.8379@gmail.com>

--- basico/03_Calculo_da_media.py
+++
@@ -1,7 +0,0 @@
-n1=int(input("Digite o primeiro número:"))
-n2=int(input("Digite o segundo número:"))
-n3=int(input("Digite o terceiro número:"))
-
-media=(n3 + n2 + n1) / 3
-
-print("A média é", media)
