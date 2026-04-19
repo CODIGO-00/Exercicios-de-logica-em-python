8d8e7d9a1d151a03a6e7ce29e1bd7eb5b9291a97
jose-programador <superacao.8379@gmail.com>
2026-04-19 18:09:56 -0300

    Criar 01-Programa-de-comparação.py
    Signed-off-by: jose-programador <superacao.8379@gmail.com>

---
+++ Estruturas-condicionais/01-Programa-de-comparação.py
@@ -0,0 +1,7 @@
+primeiro=int(input("Digite o primeiro número:"))
+segundo=int(input("Digite o segundo número:"))
+
+if primeiro >= segundo:
+    print("O primeiro número maior que o segundo.")
+else:
+	print("O primeiro número é menor que o segundo.")
