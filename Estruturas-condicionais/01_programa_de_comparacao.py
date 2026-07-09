f3a4727926c1b333f6c786a0ace41f86c54d53bf
jose-programador <Jose.silva90968@gmail.com>
2026-07-08 22:18:34 -0300

    Improve output messages for number comparison
    Signed-off-by: jose-programador <Jose.silva90968@gmail.com>

--- Estruturas-condicionais/01_programa_de_comparacao.py
+++ Estruturas-condicionais/01_programa_de_comparacao.py
@@ -2,6 +2,6 @@ primeiro=int(input("Digite o primeiro número:"))
 segundo=int(input("Digite o segundo número:"))
 
 if primeiro >= segundo:
-    print("O primeiro número maior que o segundo.")
+    print(f"O {primeiro} número maior que o {segundo}.")
 else:
-	print("O primeiro número é menor que o segundo.")
+	print(f"O {primeiro} número é menor que o {segundo}.")
