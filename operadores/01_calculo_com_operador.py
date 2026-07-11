cc496ec88618363378c8811e0993c427517d2ef6
jose-programador <Jose.silva90968@gmail.com>
2026-07-11 19:51:09 -0300

    Fix variable name from 'Número' to 'numero'
    Signed-off-by: jose-programador <Jose.silva90968@gmail.com>

--- operadores/01_calculo_com_operador.py
+++ operadores/01_calculo_com_operador.py
@@ -1,3 +1,3 @@
-Número=int(input("Digite um número:"))
-print("o Dobro é ", Número * 2)
-print("o Resultado ao quadrado é",Número ** 2)
+numero=int(input("Digite um número:"))
+print("o Dobro é", numero * 2)
+print("o Resultado ao quadrado é",numero ** 2)
